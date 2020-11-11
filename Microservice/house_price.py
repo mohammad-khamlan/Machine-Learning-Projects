470bd13ef4175533d852197e22d60d0f6fe5c222
mohammad-khamlan <47076940+mohammad-khamlan@users.noreply.github.com>
2020-11-11 20:13:49 +0200

    Update house_price.py

--- Microservice/house_price.py
+++ Microservice/house_price.py
@@ -74,6 +74,5 @@ if __name__ == '__main__':
     #Loaded model
     model = loaded_model() 
     #Run flask application
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host = '0.0.0.0', port = port)
+    app.run(host='0.0.0.0', debug=True, use_reloader=True, port=int(os.environ.get('PORT', 5000)))
 
